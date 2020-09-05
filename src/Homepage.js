22bb93ae9540bceb8560ba1c2dca1b0d8aa30484
himanshu899 <65976986+himanshu899@users.noreply.github.com>
2020-09-05 11:09:54 +0530

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-//import './Home.css';
 import Student from './Student';
 import Menu from './Menu';
-import Teahcer from './Teacher';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Teacher from './Teacher';
 import App from './App';
@@ -23,14 +21,5 @@ function Homepage() {
   );
 }
 
-const Home = () => {
-
-  return (
-     <div className="homestyle"> 
-        <h2> This is the Home Page. </h2>
-      </div>
-    )
-
-}
 
 export default Homepage;
